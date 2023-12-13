7c90ac4962e867c062e03ec7ef8d00f5af2a59fb
divorahim13 <113409028+divorahim13@users.noreply.github.com>
2023-12-13 23:59:53 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,3 +11,40 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
 };
+// Fungsi untuk memeriksa apakah elemen berada dalam tampilan pengguna
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+// Fungsi untuk mengaktifkan animasi saat elemen masuk dalam tampilan
+function animateIfInViewport() {
+    const image = document.querySelector('.holiday-img img');
+    if (isElementInViewport(image)) {
+        image.style.opacity = '1';
+    }
+}
+
+// Panggil fungsi untuk memeriksa dan mengaktifkan animasi
+window.addEventListener('scroll', animateIfInViewport);
+window.addEventListener('load', animateIfInViewport);
+
+// Fungsi untuk mengaktifkan animasi slide-in saat elemen masuk dalam tampilan
+function animateCultureIfInViewport() {
+    const cultureSection = document.querySelector('.Culture');
+    const image = document.querySelector('.Culture-img img');
+    
+    if (isElementInViewport(cultureSection)) {
+        image.style.transform = 'translateX(0)'; // Geser gambar ke dalam tampilan
+        image.style.opacity = '1'; // Atur opasitas menjadi 1
+    }
+}
+
+// Panggil fungsi untuk memeriksa dan mengaktifkan animasi saat halaman dimuat
+window.addEventListener('load', animateCultureIfInViewport);
+window.addEventListener('scroll', animateCultureIfInViewport);
