48938bdfe4d44aca4ae030bc128ae78524e7e019
divorahim13 <113409028+divorahim13@users.noreply.github.com>
2023-12-14 23:17:26 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,40 +11,54 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
 };
+
 // Fungsi untuk memeriksa apakah elemen berada dalam tampilan pengguna
-function isElementInViewport(el) {
+function isElementInViewport(el, offset = 90) {
     const rect = el.getBoundingClientRect();
     return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        rect.top >= -offset &&
+        rect.left >= -offset &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) + offset &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth) + offset
     );
 }
 
 // Fungsi untuk mengaktifkan animasi saat elemen masuk dalam tampilan
 function animateIfInViewport() {
     const image = document.querySelector('.holiday-img img');
-    if (isElementInViewport(image)) {
+    if (isElementInViewport(image, 50)) {
         image.style.opacity = '1';
     }
 }
 
-// Panggil fungsi untuk memeriksa dan mengaktifkan animasi
+// Panggil fungsi untuk memeriksa dan mengaktifkan animasi saat pengguna menggulir
 window.addEventListener('scroll', animateIfInViewport);
 window.addEventListener('load', animateIfInViewport);
 
-// Fungsi untuk mengaktifkan animasi slide-in saat elemen masuk dalam tampilan
-function animateCultureIfInViewport() {
-    const cultureSection = document.querySelector('.Culture');
-    const image = document.querySelector('.Culture-img img');
-    
+// Fungsi untuk menambah class 'in-viewport' jika elemen .Culture terlihat
+function checkVisibility() {
+    var cultureSection = document.querySelector('.Culture');
     if (isElementInViewport(cultureSection)) {
-        image.style.transform = 'translateX(0)'; // Geser gambar ke dalam tampilan
-        image.style.opacity = '1'; // Atur opasitas menjadi 1
+        cultureSection.classList.add('in-viewport');
+    }
+}
+
+// Jalankan fungsi checkVisibility saat pengguna menggulir
+window.addEventListener('scroll', checkVisibility);
+
+// Panggil checkVisibility saat halaman pertama kali dimuat
+checkVisibility();
+
+// Fungsi untuk mengaktifkan animasi saat elemen mendekati tampilan
+function animateIfNearViewport() {
+    const image = document.querySelector('.Culture-img img');
+    if (isElementInViewport(image, 100)) { // Ganti 100 dengan offset sesuai kebutuhan Anda
+        image.style.opacity = '1'; // Ganti class dengan style untuk mengubah opacity
     }
 }
 
 // Panggil fungsi untuk memeriksa dan mengaktifkan animasi saat halaman dimuat
-window.addEventListener('load', animateCultureIfInViewport);
-window.addEventListener('scroll', animateCultureIfInViewport);
+window.addEventListener('load', animateIfNearViewport);
+
+// Panggil fungsi untuk memeriksa dan mengaktifkan animasi saat pengguna menggulir
+window.addEventListener('scroll', animateIfNearViewport);
